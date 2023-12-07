980d3415a7ee2b96d0a8465e3a1143b4e7d0c12b
Emre <emreetumer0@gmail.com>
2023-12-07 21:23:02 +0300

    1.Commit
    Proje paketleri ve kullanıcı giriş sayfası ile ilgili eklemeler yapıldı.

---
+++ proje_odevi/proje_odevi/Controllers/HesapController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proje_odevi.Controllers
+{
+    public class HesapController : Controller
+    {
+        public IActionResult Giris()
+        {
+            return View();
+        }
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        public IActionResult Profil()
+        {
+            return View();
+        }
+    }
+}
