c0d4a523dfb3eb6129f2268e5d96afc6f3239789
Emre <emreetumer0@gmail.com>
2023-12-17 13:45:25 +0300

    7.Commit
    Admin sayfası oluşturuldu. Kullanıcı ve adminin görüntüleyebileceği sayfalar belirlendi.

---
+++ 7.Commit/proje_odevi/Controllers/HesapController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NETCore.Encrypt.Extensions;
+using proje_odevi.Entities;
+using proje_odevi.Models;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace proje_odevi.Controllers
+{
+    public class HesapController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+        private readonly IConfiguration _configuration;
+
+        public HesapController(DatabaseContext databaseContext, IConfiguration configuration)
+        {
+            _databaseContext = databaseContext;
+            _configuration = configuration;
+        }
+
+        public IActionResult Giris()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Giris(GirisViewModel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+                string saltedPassword = model.Sifre + md5Salt;
+                string hashedPassword = saltedPassword.MD5();
+
+                Kullanici kullanici = _databaseContext.Kullanicilar.SingleOrDefault(x => x.KullaniciAdi.ToLower() == model.KullaniciAdi.ToLower() && x.Sifre == hashedPassword);
+                if(kullanici != null)
+                {
+                    if (kullanici.Locked)
+                    {
+                        ModelState.AddModelError(nameof(model.KullaniciAdi), "Kullanıcı Kilitli");
+                        return View(model);
+                    }
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim(ClaimTypes.NameIdentifier, kullanici.Id.ToString()));
+                    claims.Add(new Claim(ClaimTypes.Name,kullanici.AdSoyad ?? string.Empty));
+                    claims.Add(new Claim(ClaimTypes.Role, kullanici.Role ));
+                    claims.Add(new Claim("KullaniciAdi",kullanici.KullaniciAdi));
+
+                    ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kullanıcı Adı Veya Şifre Hatalıdır.");
+                }
+            }
+            return View(model);
+        }
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Kayit(KayitViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_databaseContext.Kullanicilar.Any(x=>x.KullaniciAdi.ToLower()==model.KullaniciAdi.ToLower()))
+                {
+                    ModelState.AddModelError(nameof(model.KullaniciAdi), "Kullanıcı Adı Daha Önceden Vardır");
+                    View(model);
+                }
+                string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+                string saltedPassword = model.Sifre + md5Salt;
+                string hashedPassword = saltedPassword.MD5();
+
+                Kullanici kullanici = new()
+                {
+                    KullaniciAdi = model.KullaniciAdi,
+                    Sifre = hashedPassword
+                };
+                _databaseContext.Kullanicilar.Add(kullanici);
+                int affectedRowCount = _databaseContext.SaveChanges();
+                if(affectedRowCount == 0)
+                {
+                    ModelState.AddModelError("", "Hata Var");
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Giris));
+                }
+            }
+            return View(model);
+        }
+        public IActionResult Profil()
+        {
+            return View();
+        }
+        public IActionResult Cikis()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(Giris));
+        }
+    }
+}
