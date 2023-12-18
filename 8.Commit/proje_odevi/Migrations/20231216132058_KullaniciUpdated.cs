fa87e05f20fce122727e7096cb1398d7af2052fb
Emre <emreetumer0@gmail.com>
2023-12-18 11:48:15 +0300

    8.Commit
    Kullanıcı sayfası ve admin sayfasına bazı özellikler eklendi. tasarımlar ilerletildi.

---
+++ 8.Commit/proje_odevi/Migrations/20231216132058_KullaniciUpdated.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proje_odevi.Migrations
+{
+    public partial class KullaniciUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Kullanıcılar",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Kullanici");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Kullanıcılar");
+        }
+    }
+}
---
+++ 8.Commit/proje_odevi/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using proje_odevi.Entities;
+
+namespace proje_odevi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            builder.Services.AddDbContext<DatabaseContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                //opts.UseLazyLoadingProxies();
+            });
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(opts =>
+            {
+                opts.Cookie.Name = ".proje_odevi.auth";
+                opts.ExpireTimeSpan = TimeSpan.FromDays(7);
+                opts.SlidingExpiration = false;
+                opts.LoginPath = "/Hesap/Giris";
+                opts.LoginPath = "/Hesap/Cikis";
+                opts.AccessDeniedPath = "/Home/AccessDenied";
+            }
+            );
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
