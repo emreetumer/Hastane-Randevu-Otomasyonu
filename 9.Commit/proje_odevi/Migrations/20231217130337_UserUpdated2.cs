e644494cbe22c8be9ec55f95c6a1b49b066a6312
Emre <emreetumer0@gmail.com>
2023-12-19 15:34:49 +0300

    9.Commit
    Kullanıcı sayfası tasarımlarına devam edildi fotoğraf ekleme bölümü yapıldı.Veri tabanına güncellemeler yapıldı.

---
+++ 9.Commit/proje_odevi/Migrations/20231217130337_UserUpdated2.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proje_odevi.Migrations
+{
+    public partial class UserUpdated2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilResmiDosyaAdi",
+                table: "Kullanıcılar",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                defaultValue:"noimage.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilResmiDosyaAdi",
+                table: "Kullanıcılar");
+        }
+    }
+}
